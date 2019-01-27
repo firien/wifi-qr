5a6c04b3cbd0458418c0d5483ccefae19f8e0900
Patrick Griffin <halifirien@gmail.com>
2019-01-26 21:42:20 -0500

    first build

---
+++ docs/javascripts/index.96a04d476a0e8bfaf92d.js
@@ -0,0 +1,83 @@
+(function() {
+  var generate;
+
+  generate = function(text) {
+    var band, canvas, center, ctx, endArc, matrix, nub, radius, scale, size, startArc;
+    matrix = QR(text, 'H');
+    scale = 6;
+    size = matrix.length * scale;
+    canvas = document.createElement('canvas');
+    canvas.width = size;
+    canvas.height = size;
+    ctx = canvas.getContext('2d');
+    ctx.fillStyle = 'black';
+    matrix.forEach(function(row, y) {
+      return row.forEach(function(value, x) {
+        if (value) {
+          return ctx.fillRect(x * scale, y * scale, scale, scale);
+        }
+      });
+    });
+    radius = size / 2.5;
+    band = radius / 9;
+    center = [size / 2, ((size - radius) / 2) + radius - band];
+    startArc = -Math.PI / 4 * 3;
+    endArc = -Math.PI / 4;
+    ctx.fillStyle = 'white';
+    ctx.strokeStyle = 'white';
+    ctx.lineWidth = band * 2;
+    ctx.beginPath();
+    ctx.arc(center[0], center[1], radius - (band * 2), startArc, endArc);
+    ctx.lineTo(center[0], center[1]);
+    ctx.closePath();
+    ctx.fill();
+    ctx.stroke();
+    ctx.lineWidth = band;
+    ctx.strokeStyle = '#bbb';
+    ctx.beginPath();
+    ctx.arc(center[0], center[1], radius - (band * 2.5), startArc, endArc);
+    ctx.stroke();
+    ctx.beginPath();
+    ctx.arc(center[0], center[1], radius - (band * 4.5), startArc, endArc);
+    ctx.stroke();
+    ctx.beginPath();
+    ctx.arc(center[0], center[1], radius - (band * 6.5), startArc, endArc);
+    ctx.stroke();
+    ctx.fillStyle = '#bbb';
+    nub = radius - (band * 8);
+    ctx.beginPath();
+    ctx.arc(center[0], center[1], nub, startArc, endArc);
+    ctx.lineTo(center[0], center[1]);
+    ctx.closePath();
+    ctx.fill();
+    return canvas.toBlob(function(blob) {
+      var destination, image, url;
+      url = URL.createObjectURL(blob);
+      image = new Image();
+      destination = document.querySelector('#qr');
+      while (destination.firstChild) {
+        destination.removeChild(destination.firstChild);
+      }
+      destination.appendChild(image);
+      image.onload = function() {
+        this.setAttribute('height', this.naturalHeight);
+        return this.setAttribute('width', this.naturalWidth);
+      };
+      return image.src = url;
+    });
+  };
+
+  document.addEventListener('DOMContentLoaded', function() {
+    return document.querySelector('form#wifi').addEventListener('submit', function(e) {
+      var password, ssid, text;
+      e.preventDefault();
+      e.stopPropagation();
+      // https://github.com/zxing/zxing/wiki/Barcode-Contents#wi-fi-network-config-android-ios-11
+      ssid = this['ssid'].value;
+      password = this['password'].value;
+      text = `WIFI:T:WPA;S:${ssid};P:${password};;`;
+      return generate(text);
+    });
+  });
+
+}).call(this);
---
+++ docs/service.js
@@ -0,0 +1,48 @@
+(function() {
+  var $cacheName, $prefix, $urls, clearPreviousCaches, tag;
+
+  tag = '2';
+
+  $prefix = 'WIFI-QR';
+
+  $cacheName = `${$prefix}-${tag}`;
+
+  $urls = ['/wifi-qr/bundle.0d2a1e6a7f2728caebfa.js', '/wifi-qr/javascripts/index.96a04d476a0e8bfaf92d.js', '/wifi-qr/javascripts/worker.c6d7474648d703e79648.js', '/wifi-qr/stylesheets/index.f952d271a75e96112f3b.css', '/wifi-qr/images/icon-152.85033eb51371860f3cfa.png', '/wifi-qr/images/icon-167.fb225094cf8d87ef12f3.png', '/wifi-qr/images/icon-180.5eb5df2f16cbe133de6e.png', '/wifi-qr/images/icon-192.29f319cb88fabe83ff26.png', '/wifi-qr/images/icon-512.1c96c8ca1b2628918cfa.png', '/wifi-qr/pwa.701a9841b79c0debf8e2.js', '/wifi-qr/manifest.webmanifest', '/wifi-qr/index.html', '/wifi-qr/'];
+
+  self.addEventListener('install', function(event) {
+    return event.waitUntil(caches.open($cacheName).then(function(cache) {
+      return cache.addAll($urls);
+    }));
+  });
+
+  clearPreviousCaches = function() {
+    return caches.keys().then(function(keys) {
+      return Promise.all(keys.filter(function(key) {
+        return (key !== $cacheName) && key.startsWith($prefix);
+      }).map(function(key) {
+        return caches.delete(key);
+      }));
+    });
+  };
+
+  self.addEventListener('activate', function(event) {
+    return event.waitUntil(clearPreviousCaches());
+  });
+
+  self.addEventListener('fetch', function(event) {
+    return event.respondWith(caches.open($cacheName).then(function(cache) {
+      return cache.match(event.request, {
+        ignoreSearch: true
+      });
+    }).then(function(response) {
+      return response || fetch(event.request);
+    }));
+  });
+
+  self.addEventListener('message', function(event) {
+    if (event.data.action === 'skipWaiting') {
+      return self.skipWaiting();
+    }
+  });
+
+}).call(this);
