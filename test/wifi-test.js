abc7fc9de4e12fa2cebcbb1b64d6bf02c26e5e99
Patrick Griffin <58729+firien@users.noreply.github.com>
2022-11-08 21:36:45 -0500

    node v18 tests

---
+++ test/wifi-test.js
@@ -0,0 +1,9 @@
+import test from 'node:test'
+import assert from 'node:assert/strict'
+import wifiConfig from '../javascripts/wifi.js'
+
+test('escaping', () => {
+  assert.strictEqual(wifiConfig('WPA', 'asdf', 'pass'), `WIFI:T:WPA;S:asdf;P:pass;;`)
+  assert.strictEqual(wifiConfig('WPA', 'as:df', 'pass'), `WIFI:T:WPA;S:as\\:df;P:pass;;`)
+  assert.strictEqual(wifiConfig('WPA', 'as::df', 'pass'), `WIFI:T:WPA;S:as\\:\\:df;P:pass;;`)
+})
